f64db20863689511fcc5242b99ecca663ecc774a
MoritzRS <moritz.r.schulz@gmail.com>
2022-10-08 11:33:41 +0200

    added user options

--- installer/setup.sh
+++ installer/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-####################
-### Pre Setup ####
-####################
+######################
+##### Pre Setup ######
+######################
 
 # load keymap
 loadkeys de-latin1
@@ -15,3 +15,35 @@ reflector --latest 5 --country Germany,France,Switzerland,Austria --sort rate --
 
 # update packages
 pacman -Syyu --noconfirm
+
+clean
+#########################
+##### User Settings #####
+#########################
+
+# show devices
+lsblk
+
+# select device
+read -p "Drive (eg. /dev/sda) :" DRIVE
+clean
+
+# select username
+read -p "Username: " USER
+clean
+
+# select password
+read -p "Password: " PASS
+clean
+
+# confirm options
+echo "Drive: ${DRIVE}"
+echo "Username: ${USER}"
+echo "Password: ${PASS}"
+echo ""
+echo -p "Are these options correct? (y/n): " CORRECTOPTIONS
+if ["${CORRECTOPTIONS}" != "y" && "${CORRECTOPTIONS}" != "Y"]; then
+    exit 1
+fi
+clean
+echo "Starting Install Process..."
