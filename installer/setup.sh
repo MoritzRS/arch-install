53623e57d25e219e8a58dd6a38c3b546a802ce43
MoritzRS <moritz.r.schulz@gmail.com>
2022-10-08 12:20:31 +0200

    added base setup

--- installer/setup.sh
+++ installer/setup.sh
@@ -16,7 +16,7 @@ reflector --latest 5 --country Germany,France,Switzerland,Austria --sort rate --
 # update packages
 pacman -Syyu --noconfirm
 
-clean
+clear
 #########################
 ##### User Settings #####
 #########################
@@ -26,24 +26,76 @@ lsblk
 
 # select device
 read -p "Drive (eg. /dev/sda) :" DRIVE
-clean
+clear
 
 # select username
 read -p "Username: " USER
-clean
+clear
 
 # select password
 read -p "Password: " PASS
-clean
+clear
 
 # confirm options
 echo "Drive: ${DRIVE}"
 echo "Username: ${USER}"
 echo "Password: ${PASS}"
 echo ""
-echo -p "Are these options correct? (y/n): " CORRECTOPTIONS
-if ["${CORRECTOPTIONS}" != "y" && "${CORRECTOPTIONS}" != "Y"]; then
+read -p "Are these options correct? (y/n): " CORRECTOPTIONS
+if [ "${CORRECTOPTIONS}" != "y" ] && [ "${CORRECTOPTIONS}" != "Y" ]; then
     exit 1
 fi
-clean
+clear
 echo "Starting Install Process..."
+
+
+
+#######################
+##### Drive Setup #####
+#######################
+
+## partition selected device
+gdisk ${DRIVE} <<EOL
+o
+y
+n
+1
+
++500M
+ef00
+n
+2
+
++16G
+8200
+n
+3
+
+
+8304
+w
+y
+EOL
+
+## create file systems
+mkfs.fat -F32 ${DRIVE}1
+mkswap ${DRIVE}2
+mkfs.ext4 ${DRIVE}3
+
+## mount partitions
+swapon ${DRIVE}2
+mount ${DRIVE}3 /mnt
+mkdir /mnt/{boot,home}
+mkdir /mnt/boot/efi
+mount ${DRIVE}1 /mnt/boot/efi
+
+
+#############################
+##### Base Installation #####
+#############################
+
+# install system
+pacstrap /mnt base base-devel linux linux-firmware
+
+# generate fstab
+genfstab -U /mnt >> /mnt/etc/fstab
